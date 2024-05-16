f46d2ea39ae471f51f864d7326939cf8e88ff685
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-05-16 13:48:22 -0500

    got the styling of the sheet fixed

--- app/rooms/[id]/components/sheet.tsx
+++ app/rooms/[id]/components/sheet.tsx
@@ -18,14 +18,12 @@ interface SheetProps {
   onNew: (value: number) => void;
 }
 
-
 const MySheet = ({ potassium, onNew }: SheetProps) => {
-
   const [inputVal, setInputVal] = useState(0);
 
   //change this to not be any once I figure out what it is
   const handleChange = (event: any) => {
-    console.log(event.target.value)
+    console.log(event.target.value);
     onNew(event.target.value);
     console.log(potassium);
   };
@@ -34,25 +32,22 @@ const MySheet = ({ potassium, onNew }: SheetProps) => {
     <div>
       <Sheet>
         <SheetTrigger>
-            <Button>Start Here</Button>
+          <Button>Start Here</Button>
         </SheetTrigger>
         <SheetContent>
           <SheetHeader>
             <SheetTitle>Potassium Primary Fluid Adjustment</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
+            <SheetDescription></SheetDescription>
           </SheetHeader>
           {/* figure out how to add the apostrophe */}
-            <Label>Enter the patients K+ level </Label>
-            <Input
-              type="number"
-              placeholder="Enter as a decimal number"
-              onChange={handleChange}
-            />
-            {/* may not need the button because it doesnt do anything */}
-            <SheetClose />
+          <Label className="mb-2">Enter the patients K+ level </Label>
+          <Input
+            className="mt-2 mb-4"
+            type="number"
+            placeholder="Enter as a decimal number"
+            onChange={handleChange}
+          />
+          {/* may not need the button because it doesnt do anything */}
         </SheetContent>
       </Sheet>
     </div>
