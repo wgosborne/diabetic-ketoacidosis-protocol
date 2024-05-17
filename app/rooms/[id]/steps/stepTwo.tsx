4ceb2cad24d8c3a0e65ec853aeda1f39cfd449a7
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-05-17 13:16:40 -0500

    working on step 2

--- app/rooms/[id]/steps/stepTwo.tsx
+++ app/rooms/[id]/steps/stepTwo.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
 
 const StepTwo = () => {
   return (
-    <div>step Two</div>
+    <div>
+      <Card>
+          <CardHeader>
+            <CardTitle>Initial Laboratory Orders</CardTitle>
+            {/* <CardDescription>Fluid Resuscitation & Maintenance Fluids</CardDescription> */}
+          </CardHeader>
+          <CardContent>
+            <ol className='list-decimal ml-4'>
+              <li>CBC</li>
+              <li>CMP</li>
+              <li>Serum Ketones</li>
+              <li>Hgb a l c</li>
+              <li>Phosphorus</li>
+              <li>Magnesium</li>
+              <li>Urinalysis (UAI)</li>
+            </ol>
+          </CardContent>
+          {/* <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter> */}
+        </Card>
+    </div>
   )
 }
 
