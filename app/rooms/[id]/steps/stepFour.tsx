dc2ecae39e9d0ada7f4a65c794ad588b8c76026c
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-05-20 11:00:42 -0500

    deleted import

--- app/rooms/[id]/steps/stepFour.tsx
+++ app/rooms/[id]/steps/stepFour.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState } from 'react';
-import SheetFour from '../components/sheetStepFour';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { room } from '@prisma/client';
 
