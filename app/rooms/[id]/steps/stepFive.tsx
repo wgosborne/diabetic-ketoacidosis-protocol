9b7555ebc7bb84bcf6b1f5183a9ca63569611b7f
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-05-21 11:05:39 -0500

    got static steps on step 5

--- app/rooms/[id]/steps/stepFive.tsx
+++ app/rooms/[id]/steps/stepFive.tsx
@@ -17,7 +17,6 @@ const StepFive = ({
   rate,
   setRate
 }: StepFiveProps) => {
-
   return (
     <div>
       <div className="mb-3">
@@ -36,23 +35,38 @@ const StepFive = ({
               </h2>
             ) : (
               <h2 className="mb-3">Weight too low for an infusion rate</h2>
-            )}
+            )} */}
 
             <ol className="list-decimal ml-4">
               <li>
-                100 units of regular insulin in 100 mL Normal Saline = 1 unit/mL
+                RN is to document each insulin infusion rate change in the EHR
+                and MAR
               </li>
-              <li>Blood Glucose draws must be from a consistent site</li>
-              <li>DO NOT draw from a line with dextrose or TPN infusing</li>
               <li>
                 Check Blood Glucose q 1 hour (should have been ordered above)
               </li>
-              <li>Check BMP q 4 hours (should have been ordered above)</li>
-              <p className='mt-2 font-bold'>
-                **Any dose greater than 15 units per hour MUST be ordered by
-                Provider**
+              <li>
+                Blood glucose should decrease by approximately 50-100 mg/dL per
+                hour. If blood glucose decreases by more than 150 mg/dL per
+                hour, decrease the rate of change of insulin infusion by 50% and
+                notify the provider
+              </li>
+              <li>
+                When 2 CONSECUTIVE BLOOD GLUCOSES are less than 250 mg/dL (less
+                than 250 mg/dL), OR BLOOD GLUCOSE DECREASES BY MORE THAN 200
+                MG/DL IN 2 HOURS: * Change Primary Fluid to D5 1/2 NS at
+                100mL/hr. * Add Potassium Chloride per Table 1 (above)
+              </li>
+              <li>
+                Begin subcutaneous insulin 2 hours before stopping insulin drip.
+                **Do NOT abruptly stop insulin drip. Failure to overlap
+                therapies may result in recurrence of DKA. **{' '}
+              </li>
+              <p className="mt-2 font-bold">
+                ***Nurse MUST accompany patient to any diagnostic tests outside
+                of the ICU while on insulin infusion***
               </p>
-            </ol> */}
+            </ol>
           </CardContent>
           {/* <CardFooter>
               <p>Card Footer</p>
@@ -61,7 +75,6 @@ const StepFive = ({
       </div>
     </div>
   );
-  
 };
 
 export default StepFive;
