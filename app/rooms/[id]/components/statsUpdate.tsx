12704e6e55dfbe8ec92e589c4fb16c303298b7b6
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-05-23 06:18:55 -0500

    got the bones of the form up but have no submit button lmao

--- app/rooms/[id]/components/statsUpdate.tsx
+++ app/rooms/[id]/components/statsUpdate.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import axios from 'axios';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { room } from '@prisma/client';
+import { useForm } from 'react-hook-form';
 
 interface UpdateProps {
   currRoom: room;
@@ -26,31 +28,47 @@ const Update = ({
   onNewRate,
   onNewBloodGlucose
 }: UpdateProps) => {
+  const { register, handleSubmit } = useForm({});
+
+    const onSubmit = async (data: any) => {
+        //Update state everywhere
+        if(data.bloodGlucose) {
+            onNewBloodGlucose(data.bloodGlucose)
+            currRoom.bloodGlucose= data.bloodGlucose
+        }
+        
+  };
+
   return (
     <div>
-      <div className="rounded-md shadow-md flex items-center my-3 justify-center">
-        <div className='ml-3'>
-          <div className="justify-center">
-            <Label>Enter the patients Blood Glucose</Label>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)} className='rounded-md shadow-md flex items-center my-3 justify-center shadow-inner'>
+          <div className="ml-3 mt-3">
+            <div className="justify-center">
+              <Label>Enter the patients Blood Glucose</Label>
+            </div>
+            <Input
+              className="mt-2 mb-4"
+              type="number"
+              placeholder="Enter in mg/dL"
+            //   onChange={handleBloodGlucoseChange}
+            {...register("bloodGlucose", { valueAsNumber: true })}
+            
+            />
           </div>
-          <Input
-            className="mt-2 mb-4"
-            type="number"
-            placeholder="Enter in mg/dL"
-          />
-        </div>
 
-        <div className='ml-5'>
-          <div className="ml-3 justify-center">
-            <Label>Enter the patients weight</Label>
+          <div className="ml-5 mt-3">
+            <div className="ml-3 justify-center">
+              <Label>Enter the patients weight</Label>
+            </div>
+            <Input
+              className="mt-2 mb-4"
+              type="number"
+              placeholder="Enter in kilograms"
+              {...register("weight", { valueAsNumber: true })}
+            />
           </div>
-          <Input
-            className="mt-2 mb-4"
-            type="number"
-            placeholder="Enter in kilograms"
-          />
-        </div>
-
+        </form>
       </div>
     </div>
   );
