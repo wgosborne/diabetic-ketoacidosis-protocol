b79ea7d2437be9451e5044636b0068bd72862185
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-05-24 12:23:30 -0500

    started adding some types

---
+++ app/types/nonRecurring.ts
@@ -0,0 +1,10 @@
+type NonRecurring = {
+    step: number;
+    potassium: number;
+    anionGap: number;
+    weight: number;
+    rate: number;
+    bloodGlucose: number;
+    phosphorus: number;
+    BMP: number; //this one still needs to be added to the deeb
+}
