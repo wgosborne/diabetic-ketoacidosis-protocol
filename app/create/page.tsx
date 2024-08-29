4c3fbf3fe7b084646c25f5bf6098323852786c04
Wagner Osborne <wgosborne@crimson.ua.edu>
2024-08-29 13:18:49 -0500

    made create page but not adding anything rn becuase its not priority

---
+++ app/create/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Grid from '../components/grid';
+import prisma from '@/prisma/client';
+
+const Create = async () => {
+
+  return (
+    <div>
+      create page
+    </div>
+  );
+};
+
+export default Create;
